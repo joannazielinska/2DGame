74603e093de2d67a456ecb88b429c4841e3cf705
Joanna Zielinska <joanna.ewa.zielinska95@gmail.com>
2018-05-27 01:59:06 +0200

    Update

---
+++ PlatformGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var move = new Vector3(moveHorizontal, moveVertical, 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       //TODO
+    }
+}
