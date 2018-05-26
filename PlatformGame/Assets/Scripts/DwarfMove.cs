1c5d7bc0b96ec59e5ae172d88742d9c0f567002e
Joanna Byra <byra.joann@gmail.com>
2018-05-26 15:14:53 +0200

    Added dwarf sprite with jump

---
+++ PlatformGame/Assets/Scripts/DwarfMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwarfMove : MonoBehaviour {
+
+	private Rigidbody2D rigidbody2d;
+
+	public float jumpHigh;
+	// public float moveSpeed;
+	// public float friction;
+
+	void Awake() {
+		rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, 0);
+			rigidbody2d.AddForce(new Vector2(0.0f, jumpHigh));
+		}
+		
+
+	}
+}
