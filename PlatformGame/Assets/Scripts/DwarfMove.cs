172813d52044139c087beec0344384e81d7d1c83
Joanna Byra <byra.joann@gmail.com>
2018-05-27 21:13:26 +0200

    Dwarf movement

--- PlatformGame/Assets/Scripts/DwarfMove.cs
+++ PlatformGame/Assets/Scripts/DwarfMove.cs
@@ -7,8 +7,8 @@ public class DwarfMove : MonoBehaviour {
 	private Rigidbody2D rigidbody2d;
 
 	public float jumpHigh;
-	// public float moveSpeed;
-	// public float friction;
+	public float speed;
+	public float friction;
 
 	void Awake() {
 		rigidbody2d = GetComponent<Rigidbody2D>();
@@ -25,7 +25,16 @@ public class DwarfMove : MonoBehaviour {
 			rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, 0);
 			rigidbody2d.AddForce(new Vector2(0.0f, jumpHigh));
 		}
-		
+		if (Input.GetButtonDown("Horizontal")) {
+			rigidbody2d.AddForce(new Vector2(speed, 0.0f));
+			
+		}
+		// else if (rigidbody2d.velocity.x > 0) {
+		// 	rigidbody2d.AddForce(new Vector2(-friction, 0.0f));
+		// }
+		else {
+			rigidbody2d.velocity = new Vector2(0, rigidbody2d.velocity.y);
+		}
 
 	}
 }
